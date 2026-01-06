32b0d76feb419b614b919443ab578c1be10dd45a
Navanith Krishnan P K <69448465+navanithpk@users.noreply.github.com>
2026-01-06 07:36:24 +0530

    Djngo Admin can now show Questions

--- core/admin.py
+++ core/admin.py
@@ -1,29 +1,51 @@
 from django.contrib import admin
-from django.contrib import admin
-from .models import Grade, Subject, Topic, LearningObjective
+from .models import (
+    Grade,
+    Subject,
+    Topic,
+    LearningObjective,
+    Question,
+)
 
-# Register your models here.
+# ----------------------------
+# Grade
+# ----------------------------
 @admin.register(Grade)
 class GradeAdmin(admin.ModelAdmin):
     list_display = ("name",)
     search_fields = ("name",)
+
+
+# ----------------------------
+# Subject
+# ----------------------------
 @admin.register(Subject)
 class SubjectAdmin(admin.ModelAdmin):
     list_display = ("name",)
     search_fields = ("name",)
+
+
+# ----------------------------
+# Topic
+# ----------------------------
 @admin.register(Topic)
 class TopicAdmin(admin.ModelAdmin):
     list_display = ("name", "grade", "subject")
     list_filter = ("grade", "subject")
     search_fields = ("name",)
+
+
+# ----------------------------
+# Learning Objective
+# ----------------------------
 @admin.register(LearningObjective)
 class LearningObjectiveAdmin(admin.ModelAdmin):
     list_display = (
-        "grade",
-        "subject",
-        "topic",
         "code",
         "short_description",
+        "topic",
+        "subject",
+        "grade",
     )
     list_filter = ("grade", "subject", "topic")
     search_fields = ("code", "description")
@@ -33,3 +55,27 @@ class LearningObjectiveAdmin(admin.ModelAdmin):
 
     short_description.short_description = "LO Description"
 
+
+# ----------------------------
+# Question
+# ----------------------------
+@admin.register(Question)
+class QuestionAdmin(admin.ModelAdmin):
+    list_display = (
+        "id",
+        "short_question",
+        "grade",
+        "subject",
+        "topic",
+        "question_type",
+        "marks",
+        "created_by",
+    )
+    list_filter = ("grade", "subject", "topic", "question_type")
+    search_fields = ("question_text", "answer_text")
+    ordering = ("-id",)
+
+    def short_question(self, obj):
+        return obj.question_text[:80]
+
+    short_question.short_description = "Question"
